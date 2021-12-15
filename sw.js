d00c7d8679156bdcb186f524b7bb7919644673d5
study-hary-id <haryansyah.study.id@gmail.com>
2021-12-15 20:02:57 +0700

    Add activate event listener

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('install', e => {
   console.log('serviceWorker has installed.');
 });
+
+self.addEventListener('activate', e => {
+  console.log('serviceWorker has activated.');
+});
