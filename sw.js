efb6a96a00c97873caa65774040fabb307f9490b
study-hary-id <haryansyah.study.id@gmail.com>
2021-12-18 12:50:37 +0700

    Add fetch event listener

--- sw.js
+++ sw.js
@@ -5,3 +5,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   console.log('serviceWorker has activated.');
 });
+
+self.addEventListener('fetch', e => {
+  console.log('serviceWorker fetch events', e);
+});
