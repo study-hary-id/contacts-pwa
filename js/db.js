e0fcc059114b0efb76c8a15f24e1d05623c157b6
study-hary-id <haryansyah.study.id@gmail.com>
2021-12-19 00:03:08 +0700

    add delete and edit controller

--- js/db.js
+++ js/db.js
@@ -1,5 +1,15 @@
+db.enablePersistence().catch(err => {
+  if (err.code == 'failed-precondition') {
+    console.log('Multiple tabs is opened');
+  } else if (err.code == 'unimplemented') {
+    console.log('Browser is not supported');
+  }
+});
+
 const contactForm = document.querySelector('.add-contact form');
 const addContactModal = document.getElementById('add_contact_modal');
+const editForm = document.querySelector('.edit-contact form');
+const editContactModal = document.getElementById('edit_contact_modal');
 
 contactForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -18,9 +28,48 @@ contactForm.addEventListener('submit', e => {
   });
 });
 
+editForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const contact = {
+    name: editForm.name.value,
+    phone: parseInt(editForm.phone.value),
+  };
+  db.collection('contacts').doc(updateId).update(contact).then(() => {
+    var instance = M.Modal.getInstance(editContactModal);
+    editForm.reset();
+    instance.close();
+    editForm.querySelector('.error').textContent = '';
+  }).catch(err => {
+    editForm.querySelector('.error').textContent = err.message;
+  });
+});
+
 db.collection('contacts').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
-    renderContacts(change.doc.data(), change.doc.id);
+    if (change.type === 'added') {
+      renderContacts(change.doc.data(), change.doc.id);
+    } else if (change.type === 'removed') {
+      removeContact(change.doc.id);
+    }
+    // else if (change.type === 'modified') {
+//       rerenderContact(change.doc.data(), change.doc.id);
+//     }
     console.log(`${change.doc.id} has ${change.type}.`, change.doc.data());
   });
+});
+
+const contactContainer = document.querySelector('.contacts');
+contactContainer.addEventListener('click', e => {
+  if (e.target.textContent === 'delete_outline') {
+    const id = e.target.parentElement.getAttribute('data-id');
+    db.collection('contacts').doc(id).delete();
+  } else if (e.target.textContent === 'edit') {
+    // Store the data that wants to edit to the form.
+    updateId = e.target.parentElement.getAttribute('data-id');
+    const contact = document.querySelector(`.contact[data-id=${updateId}]`);
+    const name = contact.querySelector('.name').innerHTML;
+    const phone = contact.querySelector('.phone').innerHTML;
+    editForm.name.value = name;
+    editForm.phone.value = phone;
+  }
 });
--- js/ui.js
+++ js/ui.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const renderContacts = (data, id) => {
   const html = `
-    <li class="collection-item avatar" data-id=${id}>
+    <li class="collection-item contact avatar" data-id=${id}>
       <i class="material-icons circle">account_circle</i>
       <span class="title name">${data.name}</span>
       <p>Phone: <span class="phone">${data.phone}</span></p>
@@ -24,3 +24,25 @@ const renderContacts = (data, id) => {
   `
   contacts.innerHTML += html;
 };
+
+const removeContact = id => {
+  const contact = document.querySelector(`.contact[data-id=${id}]`);
+  contact.remove();
+};
+
+// const rerenderContact = (data, id) => {
+//   const contact = document.querySelector(`.contact[data-id=${id}]`);
+//   const html = `
+//     <li class="collection-item contact avatar" data-id=${id}>
+//       <i class="material-icons circle">account_circle</i>
+//       <span class="title name">${data.name}</span>
+//       <p>Phone: <span class="phone">${data.phone}</span></p>
+//       <div class="secondary-content" data-id=${id}>
+//         <i class="material-icons modal-trigger" href="#edit_contact_modal" style="cursor:pointer">edit</i>
+//         <i class="material-icons" style="cursor:pointer">${data.favorite ? "star" : "star_border"}</i>
+//         <i class="material-icons" style="cursor:pointer">delete_outline</i>
+//       </div>
+//     </li>
+//   `
+//   contacts.innerHTML += html;
+// };
